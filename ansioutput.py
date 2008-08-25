d8f8e752e4350e32e78709e0325cc524c0bd7af5
Todd Eddy <vr@vrillusions.com>
2008-08-25 04:59:19 +0000

    all files are UTF-8 encoded (without BOM) and converted tabs to spaces

--- ansioutput.py
+++ ansioutput.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# vim:ts=4:sw=4:ft=python:fileencoding=utf-8
+
 # Copyright 1998-2003 Daniel Robbins, Gentoo Technologies, Inc.
 # Distributed under the GNU Public License v2
 # $Header: /home/cvsroot/gentoo-src/portage/pym/output.py,v 1.16 2003/05/29 08:34:55 carpaski Exp $
@@ -30,69 +33,69 @@ codes["red"]="\x1b[31;01m"
 codes["darkred"]="\x1b[31;06m"
 
 def xtermTitle(mystr):
-	if havecolor and dotitles and os.environ.has_key("TERM"):
-		myt=os.environ["TERM"]
-		if myt in ["xterm","Eterm","aterm","rxvt"]:
-			sys.stderr.write("\x1b]1;\x07\x1b]2;"+str(mystr)+"\x07")
-			sys.stderr.flush()
+    if havecolor and dotitles and os.environ.has_key("TERM"):
+        myt=os.environ["TERM"]
+        if myt in ["xterm","Eterm","aterm","rxvt"]:
+            sys.stderr.write("\x1b]1;\x07\x1b]2;"+str(mystr)+"\x07")
+            sys.stderr.flush()
 
 def xtermTitleReset():
-	if havecolor and dotitles and os.environ.has_key("TERM"):
-		myt=os.environ["TERM"]
-		xtermTitle(os.environ["TERM"])
+    if havecolor and dotitles and os.environ.has_key("TERM"):
+        myt=os.environ["TERM"]
+        xtermTitle(os.environ["TERM"])
 
 
 def notitles():
-	"turn off title setting"
-	dotitles=0
+    "turn off title setting"
+    dotitles=0
 
 def nocolor():
-	"turn off colorization"
-	havecolor=0
-	for x in codes.keys():
-		codes[x]=""
+    "turn off colorization"
+    havecolor=0
+    for x in codes.keys():
+        codes[x]=""
 
 def resetColor():
-	return codes["reset"]
+    return codes["reset"]
 
 def ctext(color,text):
-	return codes[ctext]+text+codes["reset"]
+    return codes[ctext]+text+codes["reset"]
 
 def bold(text):
-	return codes["bold"]+text+codes["reset"]
+    return codes["bold"]+text+codes["reset"]
 def white(text):
-	return bold(text)
+    return bold(text)
 
 def teal(text):
-	return codes["teal"]+text+codes["reset"]
+    return codes["teal"]+text+codes["reset"]
 def turquoise(text):
-	return codes["turquoise"]+text+codes["reset"]
+    return codes["turquoise"]+text+codes["reset"]
 def darkteal(text):
-	return turquoise(text)
+    return turquoise(text)
 
 def fuscia(text):
-	return codes["fuscia"]+text+codes["reset"]
+    return codes["fuscia"]+text+codes["reset"]
 def purple(text):
-	return codes["purple"]+text+codes["reset"]
+    return codes["purple"]+text+codes["reset"]
 
 def blue(text):
-	return codes["blue"]+text+codes["reset"]
+    return codes["blue"]+text+codes["reset"]
 def darkblue(text):
-	return codes["darkblue"]+text+codes["reset"]
+    return codes["darkblue"]+text+codes["reset"]
 
 def green(text):
-	return codes["green"]+text+codes["reset"]
+    return codes["green"]+text+codes["reset"]
 def darkgreen(text):
-	return codes["darkgreen"]+text+codes["reset"]
+    return codes["darkgreen"]+text+codes["reset"]
 
 def yellow(text):
-	return codes["yellow"]+text+codes["reset"]
+    return codes["yellow"]+text+codes["reset"]
 def brown(text):
-	return codes["brown"]+text+codes["reset"]
+    return codes["brown"]+text+codes["reset"]
 def darkyellow(text):
-	return brown(text)
+    return brown(text)
 
 def red(text):
-	return codes["red"]+text+codes["reset"]
+    return codes["red"]+text+codes["reset"]
 def darkred(text):
-	return codes["darkred"]+text+codes["reset"]
+    return codes["darkred"]+text+codes["reset"]
--- timelog.py
+++ timelog.py
@@ -1,4 +1,4 @@
-﻿#!/usr/bin/env python
+#!/usr/bin/env python
 # vim:ts=4:sw=4:ft=python:fileencoding=utf-8
 """TimeLog
 
@@ -20,64 +20,64 @@ import ansioutput
 
 
 def usage():
-	"""Usage information (called with -h option)."""
-	print "Usage:  %s" % os.path.basename(sys.argv[0])
-	print ' -h  This help message'
+    """Usage information (called with -h option)."""
+    print "Usage:  %s" % os.path.basename(sys.argv[0])
+    print ' -h  This help message'
 
 
 def initDatabase(db):
-	"""Initializes the database."""
-	conn = sqlite3.connect(db)
-	c = conn.cursor()
-	c.execute('''create table if not exists timelog
-		(id integer primary key autoincrement, 
-		project text,
-		created_at text default CURRENT_TIMESTAMP,
-		note text)''')
-	conn.commit()
-	c.close()
+    """Initializes the database."""
+    conn = sqlite3.connect(db)
+    c = conn.cursor()
+    c.execute('''create table if not exists timelog
+        (id integer primary key autoincrement, 
+        project text,
+        created_at text default CURRENT_TIMESTAMP,
+        note text)''')
+    conn.commit()
+    c.close()
 
 
 def main():
-	"""The main function."""
-	args = sys.argv[1:]
-	if "-h" in args or "--help" in args:
-		usage()
-		sys.exit(2)
-	# rest of code here
-	config = ConfigParser.ConfigParser()
-	config.read('./options.cfg')
-	databaseName = config.get('main', 'database')
-	project = config.get('main', 'default project')
-	if os.path.isfile(databaseName) == False:
-		print ansioutput.fuscia('Database not found, initializing...')
-		initDatabase(databaseName)
-	conn = sqlite3.connect(databaseName)
-	c = conn.cursor()
-	t = (project,'Did something that was really important')
-	c.execute('insert into timelog (project, note) values (?, ?)', t)
-	conn.commit()
-	#c.execute('select * from timelog')
-	#for row in c:
-	#	print row
-	c.close()
-	print "done"
+    """The main function."""
+    args = sys.argv[1:]
+    if "-h" in args or "--help" in args:
+        usage()
+        sys.exit(2)
+    # rest of code here
+    config = ConfigParser.ConfigParser()
+    config.read('./options.cfg')
+    databaseName = config.get('main', 'database')
+    project = config.get('main', 'default project')
+    if os.path.isfile(databaseName) == False:
+        print ansioutput.fuscia('Database not found, initializing...')
+        initDatabase(databaseName)
+    conn = sqlite3.connect(databaseName)
+    c = conn.cursor()
+    t = (project,'Did something that was really important')
+    c.execute('insert into timelog (project, note) values (?, ?)', t)
+    conn.commit()
+    #c.execute('select * from timelog')
+    #for row in c:
+    #    print row
+    c.close()
+    print "done"
 
 
 if __name__ == "__main__":
-	try:
-		main()
-	except KeyboardInterrupt, e:
-		# Ctrl-c
-		raise e
-	except SystemExit, e:
-		# sys.exit()
-		raise e
-	except Exception, e:
-		print "ERROR, UNEXPECTED EXCEPTION"
-		print str(e)
-		traceback.print_exc()
-		sys.exit(1)
-	else:
-		# Main function is done, exit cleanly
-		sys.exit(0)
+    try:
+        main()
+    except KeyboardInterrupt, e:
+        # Ctrl-c
+        raise e
+    except SystemExit, e:
+        # sys.exit()
+        raise e
+    except Exception, e:
+        print "ERROR, UNEXPECTED EXCEPTION"
+        print str(e)
+        traceback.print_exc()
+        sys.exit(1)
+    else:
+        # Main function is done, exit cleanly
+        sys.exit(0)
